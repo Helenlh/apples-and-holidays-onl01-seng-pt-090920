db03998276f73ab7f5209c443b75a5a4c3b907fd
HELEN HARMAN <hlharman@bellsouth.net>
2020-10-03 21:59:18 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -25,7 +25,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
     holiday.to_s 
   holiday.each do
   
-  if holiday == "fourth_of_july
+  if holiday == "fourth_of_july"
     answer = Fourth_of_July[1]
     return answer 
   end
@@ -34,8 +34,9 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND  New Year's arrays
-while season == winter 
-  holiday >> [supply]
+  
+while season == "winter"
+  holiday.push [supply]
 end
 
 
@@ -43,15 +44,17 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
 
- :summer => {:memorial_day => ["BBQ"] >> supply}
+ :summer => {:memorial_day => ["BBQ"]} >> supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash 
   
-  holiday_hash => {winter => #"{holiday_name} => [supply_array]}
+  holiday_hash => {season => holiday_name => [supply_array]}
 end
+
+
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
@@ -70,20 +73,34 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 
 end
-str = "String Counting"       .inspect 
-puts str.delete "ing"
 
-holiday_hash.each do |season1, holiday1|
-  new_season1 = season1.to_s
-  new_season1.delete "=>"
-  
+
+
+holiday_hash.each do |season, holiday|
+  yield season, holiday
+  puts season + ":" /n/tholiday + ":"
+    holiday_hash.each do |holiday, supply|
+  yield supply
   
 end 
 
 
 
+holiday_hash (season, holiday)
+case characters
+  when season.start_with? ":"
+   season.delete ":" 
+  when holiday.start_with? ":" 
+   holiday.delete ":"
+  else when season.ends_with? "=>"
+    season.delete "=>"
+  end 
+    
+holiday_hash (supply) 
+supply.join ", "
+
+
 def all_holidays_with_bbq(holiday_hash)
   #return an array of holiday names (as symbols) where supply lists
   #include the string "BBQ"
-
 end 
