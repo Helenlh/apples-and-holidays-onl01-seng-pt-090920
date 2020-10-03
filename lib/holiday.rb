9c06f7e04edebe95076dc6d4c8a1aae0541659e4
HELEN HARMAN <hlharman@bellsouth.net>
2020-10-03 01:26:00 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,21 +39,23 @@ while season == winter
 end
 
 
-def add_supply_to_m
-memorial_day(holiday_hash, supply)
+def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
 
+ :summer => {:memorial_day => ["BBQ"] >> supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
-  # remember to return the updated hash
-
+  # remember to return the updated hash 
+  
+  holiday_hash => {winter => New Years => [hats, streamers, firecrackers]}
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+
 
 end
 
