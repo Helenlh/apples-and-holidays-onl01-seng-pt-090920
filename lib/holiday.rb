f51042b83f8a2bbfbd1bcc18618aeb3a58a64ed0
HELEN HARMAN <hlharman@bellsouth.net>
2020-10-03 00:37:40 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,8 +18,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
   # }
   # return the second element in the 4th of July array 
   
-  holiday_hash.each do (season, holiday)
-  holiday.each do () 
+  holiday_hash.each do |season, holiday|
+  holiday.each do | |
   
   if holiday == "fourth_of_july
     answer = fourth_of_july[1].split " "
